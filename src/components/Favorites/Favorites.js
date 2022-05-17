e0b64291f976c980b39644aa8fb50a4d4d35061e
Rena <renahuseynli01@gmail.com>
2022-05-16 20:11:04 -0700

    changed to functional components

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
-import './Favorites.css';
+import React, { useState } from "react";
+import "./Favorites.css";
 
+function Favorites() {
+  // const [title, setTitle] = useState("Новый список");
+  const [movies] = useState([
+    { imdbID: "tt0068646", title: "The Godfather", year: 1972 },
+  ]);
 
-class Favorites extends Component {
-    state = {
-        title: 'Новый список',
-        movies: [
-            { imdbID: 'tt0068646', title: 'The Godfather', year: 1972 }
-        ]
-    }
-    render() { 
-        return (
-            <div className="favorites">
-                <input value="Новый список" className="favorites__name" />
-                <ul className="favorites__list">
-                    {this.state.movies.map((item) => {
-                        return <li key={item.id}>{item.title} ({item.year})</li>;
-                    })}
-                </ul>
-                <button type="button" className="favorites__save">Сохранить список</button>
-            </div>
-        );
-    }
+  return (
+    <div className="favorites">
+      <input value="Новый список" className="favorites__name" />
+      <ul className="favorites__list">
+        {movies.map((item,i) => {
+          return (
+            <li key={i}>
+              {item.title} ({item.year})
+            </li>
+          );
+        })}
+      </ul>
+      <button type="button" className="favorites__save">
+        Сохранить список
+      </button>
+    </div>
+  );
 }
- 
+
 export default Favorites;
--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,43 +1,40 @@
-import React, { Component } from 'react';
-import './SearchBox.css';
+import React, { useState } from "react";
+import "./SearchBox.css";
 
-class SearchBox extends Component {
-    state = {
-        searchLine: ''
-    }
-    searchLineChangeHandler = (e) => {
-        this.setState({ searchLine: e.target.value });
-    }
-    searchBoxSubmitHandler = (e) => {
-        e.preventDefault();
-    }
-    render() {
-        const { searchLine } = this.state;
+function SearchBox() {
+  const [searchLine, setSearchLineChangeHandler] = useState("");
 
-        return (
-            <div className="search-box">
-                <form className="search-box__form" onSubmit={this.searchBoxSubmitHandler}>
-                    <label className="search-box__form-label">
-                        Искать фильм по названию:
-                        <input
-                            value={searchLine}
-                            type="text"
-                            className="search-box__form-input"
-                            placeholder="Например, Shawshank Redemption"
-                            onChange={this.searchLineChangeHandler}
-                        />
-                    </label>
-                    <button
-                        type="submit"
-                        className="search-box__form-submit"
-                        disabled={!searchLine}
-                    >
-                        Искать
-                    </button>
-                </form>
-            </div>
-        );
-    }
+  const onChange = (e) => {
+    setSearchLineChangeHandler(e.target.value);
+  };
+
+  const searchBoxSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="search-box">
+      <form className="search-box__form" onSubmit={searchBoxSubmitHandler}>
+        <label className="search-box__form-label">
+          Искать фильм по названию:
+          <input
+            value={searchLine}
+            type="text"
+            className="search-box__form-input"
+            placeholder="Например, Shawshank Redemption"
+            onChange={onChange}
+          />
+        </label>
+        <button
+          type="submit"
+          className="search-box__form-submit"
+          disabled={!searchLine}
+        >
+          Искать
+        </button>
+      </form>
+    </div>
+  );
 }
- 
+
 export default SearchBox;
