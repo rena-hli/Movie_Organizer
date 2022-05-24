14d501aa1ad1fa0e96a6561e663fafee830d06be
Rena <renahuseynli01@gmail.com>
2022-05-24 11:21:54 +0400

    changes

--- src/pages/ListPage/ListPage.js
+++ src/pages/ListPage/ListPage.js
@@ -1,18 +1,14 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
 import { getFavSelector } from "../../redux-manager/movie-item/selector";
-import { saveFavAction } from "../../redux-manager/movie-item/actions";
 import "./ListPage.css";
 
 function ListPage() {
-  const params = useParams();
   const favorites = useSelector(getFavSelector);
-  const favoritesTitle = useSelector(saveFavAction);
 
   return (
     <div className="list-page">
-      <h1 className="list-page__title">{favoritesTitle.payload}</h1>
+      <h1 className="list-page__title">MY LIST</h1>
       <ul>
         {favorites.map((item) => {
           return (
